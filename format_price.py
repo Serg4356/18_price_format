007f97c02f1e20314d90a042966dd5d6df211bf8
Serg4356 <asdfg111000@yandex.ru>
2019-05-08 18:40:58 +0500

    Rewrite format_price function

--- format_price.py
+++ format_price.py
@@ -19,9 +19,10 @@ def create_parser():
 def format_price(price):
     try:
         pattern = '{:,.0f}'
-        if '.' in price and int(price.split('.')[-1][:2]) > 0:
+        price = float(price)
+        if price % 1 > 0:
             pattern = '{:,.2f}'
-        return pattern.format(float(price)).replace(',', ' ')
+        return pattern.format(price).replace(',', ' ')
     except (ValueError, TypeError):
         pass
 
@@ -35,3 +36,15 @@ if __name__ == '__main__':
             print(output_price)
         else:
             print('Incorrect input')
+    print(format_price(2345))
+    print(format_price([1,2,3,4]))
+    print(format_price('1000000'))
+    print(format_price('1000000.123123123'))
+    print(format_price('1000000.0000000'))
+    print(format_price('sfesefes'))
+    print(format_price(None))
+    print(format_price(1000000.2132131))
+    print(format_price(1000000.9999))
+    print(format_price(1000000.000000))
+    print(format_price(1000000.))
+    print(format_price(format_price))
